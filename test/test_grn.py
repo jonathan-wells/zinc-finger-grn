77f1740d3764a25b67f7cb6c4ee26d8cdff45b92
Jonathan Wells <jnw72@cornell.edu>
2024-08-20 10:09:48 -0400

    Update zfnetwork.grn tests

--- test/test_grn.py
+++ test/test_grn.py
@@ -1,27 +1,75 @@
 #!/usr/bin/env python3
 
-import grn
+from zfnetwork import grn, ssa
 import unittest
 
 
 class TestNode(unittest.TestCase):
 
+    a = grn.Node('A')
+    b = grn.Node('B')
+    a.add_edge(b)
+
     def test_edge_creation(self):
-        a = grn.Node('A')
-        b = grn.Node('B')
-        a.add_edge(b)
-        self.assertEqual(len(a.input), 0)
-        self.assertEqual(len(a.output), 1)
-        self.assertEqual(len(b.input), 1)
-        self.assertEqual(len(b.output), 0)
+        self.assertEqual(len(self.a.input), 0)
+        self.assertEqual(len(self.a.output), 1)
+        self.assertEqual(len(self.b.input), 1)
+        self.assertEqual(len(self.b.output), 0)
 
     def test_degree(self):
-        a = grn.Node('A')
-        b = grn.Node('B')
-        a.add_edge(b)
-        self.assertEqual(a.degree, 1)
-        self.assertEqual(b.degree, 1)
+        self.assertEqual(self.a.degree, 1)
+        self.assertEqual(self.b.degree, 1)
+
+
+class TestEdge(unittest.TestCase):
+    
+    a = grn.Node('A', beta=1.0)
+    b = grn.Node('B', beta=1.0)
+    edge = grn.Edge(a, b, k_xy=1.0)
+
+    def test_hill_activator(self):
+        self.a.mode = 'activator'
+        self.a.pop = 0
+        self.assertEqual(self.edge.hill(), 0.0)
+        
+        self.a.pop = 1
+        self.assertEqual(self.edge.hill(), 0.5)
+        
+        self.a.pop = 10000
+        self.assertAlmostEqual(self.edge.hill(), self.b.beta, delta=1e-5)
+
+    def test_hill_repressor(self):
+        self.a.mode = 'repressor'
+        self.a.pop = 0
+        self.assertAlmostEqual(self.edge.hill(), self.b.beta, delta=1e-5)
+
+        self.a.pop = 1
+        self.assertEqual(self.edge.hill(), self.b.beta/2)
+        
+        self.a.pop = 10000
+        self.assertAlmostEqual(self.edge.hill(), 0.0, delta=1e-5)
+
+
+class TestZincFingerGRN(unittest.TestCase):
+    
+    def test_from_edge_list(self):
+        znf_grn = grn.ZincFingerGRN()
+        node_types = {1: 'TF', 2: 'ZF', 3: 'TE'}
+        edges = [(1, 2), (1, 3), (2, 3)]
+        znf_grn.from_edge_list(edges, node_types)
+        self.assertEqual(len(znf_grn.tfs), 1)
+        self.assertEqual(len(znf_grn.zfs), 1)
+        self.assertEqual(len(znf_grn.tes), 1)
+        # Check for creation of implicit heterochromatin node
+        self.assertEqual(len(znf_grn.het), 1)
+        
+        self.assertEqual(znf_grn.tfs[0].mode, 'activator')
+        self.assertEqual(znf_grn.zfs[0].mode, 'activator')
+        self.assertEqual(znf_grn.tes[0].mode, None)
+        self.assertEqual(znf_grn.het[0].mode, 'repressor')
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=3)
 
